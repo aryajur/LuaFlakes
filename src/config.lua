aa6a090632eb90443887f58a8163daad46d7b860
Milind Gupta <milind.gupta@gmail.com>
2023-11-23 21:14:23 -0800

    Added .gitignore and config.lua (for many2one script)

---
+++ src/config.lua
@@ -0,0 +1,11 @@
+--[[ Run many2one like:
+
+lua many2one.lua --wd "E:/LuaFlakes/src" "E:/LuaFlakes/src/config.lua"
+]]
+-- Config.Lua file for mainGUI
+ 
+mainFile = "LuaFlakes.lua"
+outputFile = "LuaFlakes.lua"
+deployDir = "../deploy/"
+
+clearDeployDir = true
